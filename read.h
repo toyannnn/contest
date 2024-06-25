4f3d06272126f8d8159e7f9fed605dfe2d6bcd25
Vnukochpok <167310502+Vnukochpok@users.noreply.github.com>
2024-06-25 20:25:29 +0700

    read.h

--- read.h
+++ read.h
@@ -3,11 +3,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include "work.h"
-struct CommandPair {
+struct CommandPair {  //структура для хранения пар команд (длинная и короткая форма)
     char long_form[20];
     char short_form[4];
 };
-struct CommandPair commands[] = {
+struct CommandPair commands[] = {  //массив структур, содержащих длинные и короткие формы команд
     {"--create", "-c"},
     {"--extract", "-x"},
     {"--list", "-l"},
@@ -18,7 +18,7 @@ struct CommandPair commands[] = {
     {"--verbose", "-v"},
     {"--size", "-s"}
 };
-const char* process_command(const char* input_command) {
+const char* process_command(const char* input_command) {  //функция для обработки введенной команды и возврата короткой формы команды
     for (int i = 0; i < sizeof(commands) / sizeof(commands[0]); i++) {
         if (strcmp(input_command, commands[i].long_form) == 0 ||
             strcmp(input_command, commands[i].short_form) == 0) {
@@ -27,6 +27,7 @@ const char* process_command(const char* input_command) {
     }
     return "The wrong command. Type -h or --help for help.";
 }
+//функция для проверки наличия обязательных параметров
 int checkRequiredParams(int ArgsCount, char* Args[]) {
     int c_found = 0;
     int a_found = 0;
@@ -98,17 +99,19 @@ int checkRequiredParams(int ArgsCount, char* Args[]) {
         return 0; 
     }
 }
-typedef struct Param {
+
+typedef struct Param {  //определение структуры Param для хранения имени и значения параметра
     char* name;
     char* value;
 } Param;
-Param enter_params(char* name, char* value) {
+
+Param enter_params(char* name, char* value) {  //функция для создания экземпляра структуры Param с указанным именем и значением
     Param param;
     param.name = name;
     param.value = value;
     return param;
 }
-char* equalizeParams(char* param, const int params_storage_length, const char* params_storage[]) {
+char* equalizeParams(char* param, const int params_storage_length, const char* params_storage[]) {  //функция для замены длинной формы команды на короткую форму из заданного массива структур
     for (int i = 0; i < params_storage_length; i += 2) {
         if (strcmp(param, params_storage[i]) == 0) {
             return (char*)params_storage[i + 1];
@@ -116,7 +119,7 @@ char* equalizeParams(char* param, const int params_storage_length, const char* p
     }
     return param;
 }
-int countParams(int arg_num, char* arg[]) {
+int countParams(int arg_num, char* arg[]) {  //функция для подсчета количества параметров, начинающихся с символа '-'
     int params_count = 0;
     for (int i = 1; i < arg_num; i++) {
         if (arg[i][0] == '-') {
@@ -125,20 +128,20 @@ int countParams(int arg_num, char* arg[]) {
     }
     return params_count;
 }
-Param* parametersReader(int ArgsCount, char* Args[], const int params_storage_length, const char* params_storage[]) {
-    for (int i = 1; i < ArgsCount; i++) {
-        if (strncmp(Args[i], "--", 2) == 0) {
-            Args[i] = equalizeParams(Args[i], params_storage_length, params_storage);
+Param* parametersReader(int ArgsCount, char* Args[], const int params_storage_length, const char* params_storage[]) {  //функция для чтения и обработки введенных параметров
+    for (int i = 1; i < ArgsCount; i++) {  //цикл по всем введенным аргументам, начиная с 1
+        if (strncmp(Args[i], "--", 2) == 0) {  //проверка на совпадение аргумента с "--" в начале
+            Args[i] = equalizeParams(Args[i], params_storage_length, params_storage);  //вызов функции для приведения аргументов к стандартному виду
         }
     }
-    int params_count = (ArgsCount - 1) / 2;
+    int params_count = (ArgsCount - 1) / 2;  //вычисление количества параметров на основе числа аргументов
     Param* entered_params = (Param*)malloc(params_count * sizeof(Param));
     if (entered_params == NULL) {
-        perror("Memory allocation error");
+        perror("Memory allocation error");  //вывод ошибки в случае неудачного выделения памяти
         return NULL;
     }
     for (int i = 0, j = 1; i < params_count; i++, j += 2) {
-        entered_params[i] = enter_params(Args[j], Args[j + 1]);
+        entered_params[i] = enter_params(Args[j], Args[j + 1]);  //заполнение структур параметров данными из аргументов
     }
-    return entered_params;
+    return entered_params;  //возврат заполненного массива структур параметров
 }
